3221e2c7433f504c9e2d12885c870fed283c18f1
s-m-sharjeel <smsharjeel2004@gmail.com>
2024-12-08 01:17:00 +0500

    removed unnecessary Backend folder

--- Backend/Routes/Cattle/bullRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const bullController = require("../../Controllers/Cattle/bullController");
-
-//http://localhost:3000/api/bulls
-router.post("/bulls", bullController.addBull);
-
-module.exports = router;
--- Backend/Routes/Cattle/cowRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cowController = require("../../Controllers/Cattle/cowController");
-
-//http://localhost:3000/api/cows
-router.post("/cows", cowController.addCow);
-router.put("/cows/", cowController.updateLactationStatus);
-
-module.exports = router;
